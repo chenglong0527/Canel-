2ded1d07684dd3e599d36e00f44d12f3b428f458
e <1907667150@qq.com>
2020-05-10 17:51:40 +0800

    first commit

--- src/test/java/com/chenglong/canel/RocketMQConsumer.java
+++ src/test/java/com/chenglong/canel/RocketMQConsumer.java
@@ -22,12 +22,16 @@ public class RocketMQConsumer {
                                              public ConsumeOrderlyStatus consumeMessage(List<MessageExt> list, ConsumeOrderlyContext context) {
                                                  context.setAutoCommit(true);
                                                  try {
+
                                                      //对有序的消息进行处理
                                                      if (list != null && !list.isEmpty()) {
                                                          for (MessageExt message : list) {
                                                              byte[] body = message.getBody();
                                                              String s = new String(body);
                                                              System.out.println(s);
+
+//                                                             int i=1/0;
+                                                             System.out.println("处理了");
 //                                                             System.out.println("备份数据库入库");
                                                              SQLOperation.operateSQL(s);
                                                          }
@@ -36,6 +40,7 @@ public class RocketMQConsumer {
                                                  } catch (Exception e) {
                                                      //如果消息处理有问题
                                                      //返回一个状态，让他暂停一会再处理
+                                                     System.out.println("啦啦啦处理失败了等会再来处理哦！！！");
                                                      return ConsumeOrderlyStatus.SUSPEND_CURRENT_QUEUE_A_MOMENT;
                                                  }
                                                  return null;
